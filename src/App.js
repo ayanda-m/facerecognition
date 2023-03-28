c544652330db42e4658cd54a617193b96ae411e1
Ayanda Mathiso <mathisoayanda@gmail.com>
2023-03-28 22:24:12 +0200

    added particles background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/nav/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForn/ImageLinkForm';
+import Particle from './components/particles/Particle';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/* <FaceRecognition/> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = event => {
+    console.log(event);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particle />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
