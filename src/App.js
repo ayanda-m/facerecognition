a297a4aba82a5284264650b06941b28be9bf6d1a
Ayanda Mathiso <mathisoayanda@gmail.com>
2023-03-31 16:37:15 +0200

    connected the face recognition API

--- src/App.js
+++ src/App.js
@@ -4,21 +4,59 @@ import Navigation from './components/nav/Navigation';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForn/ImageLinkForm';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import Particle from './components/particles/Particle';
+
+const returnClarifaiRequest = (imageUrl) => {
+  const PAT = '1f781f6365154edea6efdd6011b65eaa';
+  const USER_ID = 'amathiso';
+  const APP_ID = 'my-first-application';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+  return requestOptions;
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = event => {
-    console.log(event);
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequest(this.state.input))
+      .then(response => response.json())
     console.log('click');
 
   }
@@ -31,7 +69,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        {/* <FaceRecognition/> */}
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
